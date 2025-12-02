197d985babb7d4354270749a52b81fd23a8277fd
pedrolgomess <pedro.lucas@guaraves.com.br>
2025-12-02 11:26:47 -0300

    Botão para sair da rotina

--- credito-guaraves-meu-bom-card/src/app/app.component.ts
+++ credito-guaraves-meu-bom-card/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { PoMenuItem } from '@po-ui/ng-components';
 import { NovoColaboradorComponent } from './novo-colaborador/novo-colaborador.component';
-
+import { ProAppConfigService } from '@totvs/protheus-lib-core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,18 +12,24 @@ export class AppComponent {
   @ViewChild(NovoColaboradorComponent)
   novoColaboradorComp!: NovoColaboradorComponent;
   
+  constructor(private proAppConfigService: ProAppConfigService) {
+  }
  menus: Array<PoMenuItem> = [
-    { label: 'Cadastrar Novo Colaborador',  action: () => this.novoColaboradorComp.abrirModal(), icon: 'an an-user', shortLabel: 'Cadastrar Novo Colaborador' },
-    {
-      label: 'Exportar Relatório',
-       action: this.onClick.bind(this),
-      icon: 'an an-clock',
-      shortLabel: 'Exportar Relatório',
-    },
+    { label: 'Cadastrar Novo Colaborador',  action: () => this.novoColaboradorComp.abrirModal(),  icon: 'an an-user',   shortLabel: 'Cadastrar Novo Colaborador'  },
+    { label: 'Exportar Relatório',          action: () => this.onClick(),                         icon: 'an an-clock',  shortLabel: 'Exportar Relatório'          },
+    { label: 'Sair da Rotina',              action: () => this.closeApp(),                        icon: 'an an-user',   shortLabel: 'Sair da Rotina'              },
 
   ];
 
   private onClick() {
-    alert('Clicked in menu item');
+    alert('Função em Desenvolvimento');
+  }
+  
+  private closeApp() {
+    if (this.proAppConfigService.insideProtheus()) {
+      this.proAppConfigService.callAppClose();
+    } else {
+      alert('O App não está sendo executado dentro do Protheus.');
+    }
   }
 }
